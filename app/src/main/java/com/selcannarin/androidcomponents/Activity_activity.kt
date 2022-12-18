f7112bde96fd92bf94b53879434b8b78e57e31db
Selcan Narin <selcannarin12@gmail.com>
2022-12-19 01:55:52 +0300

    Adding service

--- app/src/main/java/com/selcannarin/androidcomponents/Activity_activity.kt
+++ app/src/main/java/com/selcannarin/androidcomponents/Activity_activity.kt
@@ -2,10 +2,19 @@ package com.selcannarin.androidcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Activity_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activity)
+        val b = findViewById<View>(R.id.button_start) as Button
+        // setting on click event listener
+        b.setOnClickListener { Toast.makeText(applicationContext, "Activity is working.", Toast.LENGTH_LONG).show() }
     }
+
+
 }
+
--- app/src/main/java/com/selcannarin/androidcomponents/Activity_service.kt
+++ app/src/main/java/com/selcannarin/androidcomponents/Activity_service.kt
@@ -1,11 +1,27 @@
 package com.selcannarin.androidcomponents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Activity_service : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+
+        val start = findViewById<View>(R.id.button_start) as Button
+        // setting on click event listener
+        start.setOnClickListener {
+            startService(Intent(this, NewService::class.java))
+        }
+
+        val stop = findViewById<View>(R.id.button_stop) as Button
+        // setting on click event listener
+        stop.setOnClickListener {
+            stopService(Intent(this, NewService::class.java))
+        }
+
     }
 }
