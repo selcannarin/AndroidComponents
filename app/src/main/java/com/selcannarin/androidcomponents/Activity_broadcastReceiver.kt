ec4c8ee32f72072d943d90f3ff49943233342693
Selcan Narin <selcannarin12@gmail.com>
2022-12-19 01:11:22 +0300

    Adding activities

---
+++ app/src/main/java/com/selcannarin/androidcomponents/Activity_broadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.selcannarin.androidcomponents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_broadcastReceiver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_broadcast_receiver)
+    }
+}
--- app/src/main/java/com/selcannarin/androidcomponents/MainActivity.kt
+++ app/src/main/java/com/selcannarin/androidcomponents/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.selcannarin.androidcomponents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val b1 = findViewById<View>(R.id.button_activity) as Button
+        // setting on click event listener
+        b1.setOnClickListener {
+            val intent = Intent(this@MainActivity, Activity_activity::class.java)
+            startActivity(intent)
+        }
+
+        val b2 = findViewById<View>(R.id.button_service) as Button
+        // setting on click event listener
+        b2.setOnClickListener {
+            val intent = Intent(this@MainActivity, Activity_service::class.java)
+            startActivity(intent)
+        }
+
+        val b3 = findViewById<View>(R.id.button_contentP) as Button
+        // setting on click event listener
+        b3.setOnClickListener {
+            val intent = Intent(this@MainActivity, Activity_contentProvider::class.java)
+            startActivity(intent)
+        }
+
+        val b4 = findViewById<View>(R.id.button_broadcastR) as Button
+        // setting on click event listener
+        b4.setOnClickListener {
+            val intent = Intent(this@MainActivity, Activity_broadcastReceiver::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
+
+
+
+
